d6de69e726b17d683a4a999faa18c4ae30f88648
stryku <stryku2393@gmail.com>
2018-03-19 22:02:15 -0700

    green Lexer GetToken_Empty_GetInvalid test

---
+++ source/lexer/token.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace cmsl
+{
+    namespace lexer
+    {
+        struct token
+        {
+            bool is_valid() const
+            {
+                return false;
+            }
+        };
+    }
+}
