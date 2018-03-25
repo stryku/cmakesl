819e531dfcd2a0cbfa64aa58a6ffdc9c0f9d54fc
stryku <stryku2393@gmail.com>
2018-03-25 14:17:51 -0700

    chaged lexer interface to only lex() method

--- source/lexer/lexer.cpp
+++ source/lexer/lexer.cpp
@@ -5,6 +5,11 @@ namespace cmsl
 {
     namespace lexer
     {
+        lexer::tokens_container_t lexer::lex(const std::string)
+        {
+            return lexer::tokens_container_t{};
+        }
+
         token::token lexer::get_token(const std::string& source) const
         {
             return token::token{};
