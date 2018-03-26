fe2e4862f7cdf6260a87c4fc305b8d0354667e2a
stryku <stryku2393@gmail.com>
2018-03-25 21:45:53 -0700

    green Lexer/LexOperator test

--- source/lexer/lexer.hpp
+++ source/lexer/lexer.hpp
@@ -1,19 +1,16 @@
 #pragma once
 
 #include "common/string.hpp"
+#include "lexer/token/token.hpp"
 
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 namespace cmsl
 {
     namespace lexer
     {
-        namespace token
-        {
-            class token;
-        }
-
         class lexer
         {
         private:
@@ -29,13 +26,39 @@ namespace cmsl
             tokens_container_t lex();
 
         private:
+            struct aritmetical_token_definition
+            {
+                token_t::token_type_t single;
+                token_t::token_type_t op_equal;
+                token_t::token_type_t twice;
+
+                bool has_twice() const
+                {
+                    return twice != token_t::token_type_t::undef;
+                }
+            };
+
+            using aritmetical_token_definition_t = std::unordered_map<char, aritmetical_token_definition>;
+
+            aritmetical_token_definition_t create_arithmetical_token_definitions() const;
+
             token_t get_next_token();
             token_t get_numeric_token();
+            token_t get_identifier_token();
+
+            token_t get_equal_token();
+            token_t get_minus_token();
+
+            token_t get_arithmetical_token(char operator_char);
 
             void consume_integer();
+            void consume_char();
 
             bool is_end() const;
             bool is_next() const;
+
+            bool is_identifier_char(char c) const;
+            bool is_arithmetical_operator(char c) const;
 
             char current() const;
             char next() const;
@@ -43,6 +66,8 @@ namespace cmsl
         private:
             const source_t m_source;
             source_it_t m_current_pos;
+
+            const aritmetical_token_definition_t m_aritmetical_token_definitions;
         };
     }
 }
