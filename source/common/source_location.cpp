b86b0885f3e571e9eb5358aacde420adcfc678c6
stryku <stryku2393@gmail.com>
2018-04-15 03:40:39 +0200

    introduce SourceLocation/ncrementEmpty_Zero test

--- source/common/source_location.cpp
+++ source/common/source_location.cpp
@@ -1,19 +1,34 @@
 #include "common/source_location.hpp"
 
+#include <algorithm>
+
 namespace cmsl
 {
     size_t source_location::line() const
     {
-        return 0u;
+        return m_line;
     }
 
     size_t source_location::column() const
     {
-        return 0u;
+        return m_column;
     }
 
     size_t source_location::absolute() const
     {
-        return 0u;
+        return m_absolute;
+    }
+
+    bool source_location::is_at_end() const
+    {
+        return std::next(m_source.begin(), m_absolute) == m_source.end();
+    }
+
+    source_location& source_location::operator++()
+    {
+        if (is_at_end())
+        {
+            return *this;
+        }
     }
 }
--- source/common/source_location.hpp
+++ source/common/source_location.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "common/string.hpp"
+
 namespace cmsl
 {
     class source_location
@@ -8,5 +10,15 @@ namespace cmsl
         size_t line() const;
         size_t column() const;
         size_t absolute() const;
+
+        source_location& operator++();
+
+    private:
+        bool is_at_end() const;
+
+        size_t m_line{ 1u };
+        size_t m_column{ 1u };
+        size_t m_absolute{ 0u };
+        cmsl::string_view m_source;
     };
 }
