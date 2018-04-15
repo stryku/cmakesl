270f7a927a679ba05f063ede3a6671b5ec42afe9
stryku <stryku2393@gmail.com>
2018-04-15 03:30:03 +0200

    green SourceLocation/New_Zero test

---
+++ source/common/source_location.cpp
@@ -0,0 +1,19 @@
+#include "common/source_location.hpp"
+
+namespace cmsl
+{
+    size_t source_location::line() const
+    {
+        return 0u;
+    }
+
+    size_t source_location::column() const
+    {
+        return 0u;
+    }
+
+    size_t source_location::absolute() const
+    {
+        return 0u;
+    }
+}
