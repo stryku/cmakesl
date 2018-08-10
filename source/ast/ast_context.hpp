8834967dac3182640b4b46de11a98dd57a576a89
stryku <stryku2393@gmail.com>
2018-08-10 19:10:30 +0200

    added add_function() method

--- source/ast/ast_context.hpp
+++ source/ast/ast_context.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ast/type.hpp"
+#include "ast/ast_node.hpp"
 #include "common/string.hpp"
 
 #include <memory>
@@ -10,6 +11,8 @@ namespace cmsl
 {
     namespace ast
     {
+        class ast_node;
+
         class ast_context
         {
         public:
@@ -19,9 +22,12 @@ namespace cmsl
             void add_type(std::unique_ptr<type> t);
             type* find_type(cmsl::string_view name);
 
+            void add_function(std::unique_ptr<ast_node> fun);
+
         private:
             ast_context* m_parent;
             std::vector<std::unique_ptr<type>> m_types;
+            std::vector<std::unique_ptr<ast_node>> m_functions;
         };
     }
 }
