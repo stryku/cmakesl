bdf3f555dfcb4981e0dbf3b260c1446e4180b792
stryku <stryku2393@gmail.com>
2018-08-12 14:41:52 +0200

    removed commented out code

--- source/ast/ast_builder.cpp
+++ source/ast/ast_builder.cpp
@@ -7,8 +7,6 @@
 
 //#include "ast/binary_operator.hpp"
 //#include "ast/integral_constant.hpp"
-//#include "ast/infix_node.hpp"
-//#include "ast/block_node.hpp"
 #include "ast/type.hpp"
 #include "ast/function_node.hpp"
 
@@ -25,38 +23,6 @@ namespace cmsl
             m_contextes.emplace_back(std::move(builtin_ctx));
         }
 
-       /* bool ast_builder::is_constant_expression_token(const token_t& token)
-        {
-            const auto type = token.get_type();
-            return type == lexer::token::token_type::integer ||
-                type == lexer::token::token_type::real;
-        }
-
-        bool ast_builder::is_binary_operator_token(const token_t& token)
-        {
-            const auto type = token.get_type();
-            return type == lexer::token::token_type::plus ||
-                type == lexer::token::token_type::minus;
-        }
-
-        bool ast_builder::is_infix_token(const token_t& token)
-        {
-            const auto allowed_tokens = {
-                token_t::token_type_t::integer,
-                token_t::token_type_t::real,
-                token_t::token_type_t::plus,
-                token_t::token_type_t::minus,
-                token_t::token_type_t::open_paren,
-                token_t::token_type_t::close_paren
-            };
-
-            return std::any_of(std::cbegin(allowed_tokens), std::cend(allowed_tokens),
-                               [&token](const auto token_type)
-                               {
-                                   return token.get_type() == token_type;
-                               });
-        }*/
-
         std::unique_ptr<ast_context> ast_builder::build(ast_context& builtin_ast_ctx,
                                                         errors::errors_observer& err_observer,
                                                         const tokens_container_t& tokens)
@@ -66,9 +32,10 @@ namespace cmsl
 
             auto fun = m_parser->get_function(*ctx);
 
-            if (fun)
+            while(fun)
             {
                 ctx->add_function(std::move(fun));
+                fun = m_parser->get_function(*ctx);
             }
 
             return std::move(ctx);
