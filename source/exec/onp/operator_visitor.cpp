151201d6c77fbe985d116aa0bb2afa94ed1e94c1
stryku <stryku2393@gmail.com>
2018-08-20 14:52:19 +0200

    handling plus and dot operators in operator_visitor

--- source/exec/onp/operator_visitor.cpp
+++ source/exec/onp/operator_visitor.cpp
@@ -20,29 +20,54 @@ namespace cmsl
             operator_visitor::instance_t* operator_visitor::operator()(instance_t* lhs, instance_t* rhs)
             {
                 // todo handle all operators
-                const auto val = lhs->get_value() + rhs->get_value();
-                return m_instances.create(val);
+                switch(m_operator)
+                {
+                    case token_type_t::plus: return handle_plus_operator(lhs, rhs);
+                    case token_type_t::dot: return nullptr; //todo raise error
+                }
             }
 
-            operator_visitor::instance_t* operator_visitor::operator() (instance_t* lhs, const token_t& token)
+            operator_visitor::instance_t* operator_visitor::operator() (instance_t* lhs, const token_t& rhs)
             {
-                // todo handle all operators
-                return lhs->get_member(token.str());
+                switch(m_operator)
+                {
+                    case token_type_t::plus: return handle_plus_operator(lhs, get_instance(rhs));
+                    case token_type_t::dot: return handle_dot_operator(lhs, rhs);
+                }
             }
 
             operator_visitor::instance_t* operator_visitor::operator()(const token_t& lhs, const token_t& rhs)
             {
-                // todo handle all operators
-                auto lhs_instance = m_exec_ctx.get_variable(lhs.str());
-                return lhs_instance->get_member(rhs.str());
+                switch(m_operator)
+                {
+                    case token_type_t::plus: return handle_plus_operator(get_instance(lhs), get_instance(rhs));
+                    case token_type_t::dot: return handle_dot_operator(get_instance(lhs), rhs);
+                }
             }
 
             operator_visitor::instance_t* operator_visitor::operator()(const token_t& lhs, instance_t* rhs)
             {
-                // todo handle all operators
-                auto lhs_instance = m_exec_ctx.get_variable(lhs.str());
-                const auto val = lhs_instance->get_value() + rhs->get_value();
+                switch(m_operator)
+                {
+                    case token_type_t::plus: return handle_plus_operator(get_instance(lhs), rhs);
+                    case token_type_t::dot: return nullptr; // todo raise error
+                }
+            }
+
+            operator_visitor::instance_t* operator_visitor::handle_plus_operator(instance_t* lhs, instance_t* rhs)
+            {
+                const auto val = lhs->get_value() + rhs->get_value();
                 return m_instances.create(val);
+            }
+
+            operator_visitor::instance_t* operator_visitor::handle_dot_operator(instance_t* lhs, const token_t& rhs)
+            {
+                return lhs->get_member(rhs.str());
+            }
+
+            operator_visitor::instance_t* operator_visitor::get_instance(const token_t& token)
+            {
+                return m_exec_ctx.get_variable(token.str());
             }
         }
     }
