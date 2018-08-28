b27c99cb16975986dff669bd1b441bbc95b4e141
stryku <stryku2393@gmail.com>
2018-08-29 00:05:40 +0200

    introduce while_statement

---
+++ source/exec/stmt/while_statement.cpp
@@ -0,0 +1,12 @@
+#include "exec/stmt/while_statement.hpp"
+
+namespace cmsl
+{
+    namespace exec
+    {
+        namespace stmt
+        {
+
+        }
+    }
+}
---
+++ source/exec/stmt/while_statement.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "exec/stmt/statement.hpp"
+
+namespace cmsl
+{
+    namespace exec
+    {
+        namespace stmt
+        {
+            class while_statement : public statement
+            {
+
+            };
+        }
+    }
+}
