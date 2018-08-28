47700a3390256ea050e426b05fa6bc4351dc10d8
stryku <stryku2393@gmail.com>
2018-08-28 23:45:38 +0200

    introduce while_node

---
+++ source/ast/while_node.cpp
@@ -0,0 +1,1 @@
+#include "ast/while_node.hpp"
---
+++ source/ast/while_node.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ast/ast_node.hpp"
+
+namespace cmsl
+{
+    namespace ast
+    {
+        class while_node : public ast_node
+        {
+        public:
+
+        };
+    }
+}
