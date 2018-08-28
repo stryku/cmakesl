619f77f1864b160f219a6eac463c08fe6206ada6
stryku <stryku2393@gmail.com>
2018-08-28 22:41:40 +0200

    introduce if_else_statement

---
+++ source/exec/stmt/if_else_statement.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "exec/stmt/statement.hpp"
+
+namespace cmsl
+{
+    namespace exec
+    {
+        namespace stmt
+        {
+            class if_else_statement : public statement
+            {
+
+            };
+        }
+    }
+}
+
+
