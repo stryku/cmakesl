42827a7adf06f1a99c30a72ef8cb706e73bf0373
stryku <stryku2393@gmail.com>
2018-08-31 19:14:07 +0200

    moved conversion_visitor to its own file

--- source/exec/instance/instance_value.hpp
+++ source/exec/instance/instance_value.hpp
@@ -2,13 +2,15 @@
 
 #include <boost/variant.hpp>
 
+#include <string>
+
 namespace cmsl
 {
     namespace exec
     {
         namespace inst
         {
-            using instance_value_t = boost::variant<bool, int, double>;
+            using instance_value_t = boost::variant<bool, int, double, std::string>;
         }
     }
 }
