68bb45b66621326bf9110ab7bf5f4de0d7dd3d9e
stryku <stryku2393@gmail.com>
2018-08-31 17:22:56 +0200

    added more tests for instance_converter

--- test/exec/instance_converter_test.cpp
+++ test/exec/instance_converter_test.cpp
@@ -15,22 +15,63 @@ namespace cmsl
             using instances_holder_t = cmsl::exec::inst::instances_holder;
             using instance_value_t = cmsl::exec::inst::instance_value_t ;
 
-            TEST(InstanceConverter_ConvertToType, FundamentalArithmetic_GetCorrectValue)
+            namespace convert_to_type
             {
-                // INTRODUCE AST AND EXEC CONTEXT PROVIDER INTERFACE AND USE IT INSTEAD OF EXEC
-
-                test_context_provider ctx_provider;
-                instances_holder_t instances{ ctx_provider };
-                instance_converter_t converter{ instances };
-
+                struct ConvertToType_FundamentalArithmetic_State
                 {
-                    instance_value_t val = true;
-                    auto instance = instances.create(val);
-                    const auto desired_type = ctx_provider.get_ast_ctx().find_type("int");
-                    auto converted = converter.convert_to_type(instance, *desired_type);
-                    auto converted_value = boost::get<int>(converted->get_value());
-                    ASSERT_THAT(converted_value, static_cast<int>(true));
+                    instance_value_t value_to_convert;
+                    instance_value_t expected_bool_value;
+                    instance_value_t expected_int_value;
+                    instance_value_t expected_double_value;
+                };
+
+                struct ConvertToType : public testing::TestWithParam<ConvertToType_FundamentalArithmetic_State>
+                {};
+
+                TEST_P(ConvertToType, FundamentalArithmetic_GetCorrectValue)
+                {
+                    test_context_provider ctx_provider;
+                    instances_holder_t instances{ctx_provider};
+                    instance_converter_t converter{instances};
+
+                    const auto& state = GetParam();
+
+                    auto instance_to_convert = instances.create(state.value_to_convert);
+
+                    // to bool
+                    {
+                        const auto desired_type = ctx_provider.get_ast_ctx() .find_type("bool");
+                        auto converted_instance = converter.convert_to_type(instance_to_convert, *desired_type);
+                        auto converted_value = converted_instance->get_value();
+                        ASSERT_THAT(converted_value, state.expected_bool_value);
+                    }
+
+                    // to int
+                    {
+                        const auto desired_type = ctx_provider.get_ast_ctx() .find_type("int");
+                        auto converted_instance = converter.convert_to_type(instance_to_convert, *desired_type);
+                        auto converted_value = converted_instance->get_value();
+                        ASSERT_THAT(converted_value, state.expected_int_value);
+                    }
+
+                    // to double
+                    {
+                        const auto desired_type = ctx_provider.get_ast_ctx() .find_type("double");
+                        auto converted_instance = converter.convert_to_type(instance_to_convert, *desired_type);
+                        auto converted_value = converted_instance->get_value();
+                        ASSERT_THAT(converted_value, state.expected_double_value);
+                    }
                 }
+
+                const auto values = testing::Values(
+                        ConvertToType_FundamentalArithmetic_State{ true, true, 1, 1.0 },
+                        ConvertToType_FundamentalArithmetic_State{ false, false, 0, 0.0 },
+                        ConvertToType_FundamentalArithmetic_State{ 0.9, true, 0, 0.9 },
+                        ConvertToType_FundamentalArithmetic_State{ 1.9, true, 1, 1.9 },
+                        ConvertToType_FundamentalArithmetic_State{ 2.9, true, 2, 2.9 }
+                );
+
+                INSTANTIATE_TEST_CASE_P(InstanceConverter, ConvertToType, values);
             }
         }
     }
