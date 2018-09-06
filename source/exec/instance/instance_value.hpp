e6a9f95520348b7f6a1a9d4b320cdb9e2b804fe4
stryku <stryku2393@gmail.com>
2018-09-06 18:55:50 +0200

    moved generic_instance_value to its own files

--- source/exec/instance/instance_value.hpp
+++ source/exec/instance/instance_value.hpp
@@ -1,16 +1,9 @@
 #pragma once
 
-#include <boost/variant.hpp>
-
-#include <string>
-#include <list>
+#include "exec/instance/generic_instance_value.hpp"
 
 namespace cmsl
 {
-    namespace ast
-    {
-        class type;
-    }
 
     namespace exec
     {
@@ -27,61 +20,7 @@ namespace cmsl
                 generic
             };
 
-            class generic_instance_value
-            {
-            public:
-                using instance_ptr_t = std::unique_ptr<instance>; //TODO CONSIDER POINTERS TO INSTANCES
-                using list_t = std::list<instance_ptr_t>;
-                using generic_variant_t = boost::variant<list_t>;
-
-                enum class generic_instance_value_type
-                {
-                    list
-                };
-
-                explicit generic_instance_value(const ast::type& t, generic_instance_value_type value_type);
-
-                generic_instance_value(const generic_instance_value& other);
-                generic_instance_value& operator=(const generic_instance_value& other);
-
-                bool operator==(const generic_instance_value& rhs) const;
-
-                const ast::type& get_type() const;
-
-                template <typename Callable>
-                auto apply(Callable&& cb)
-                {
-                    return boost::apply_visitor(cb, m_generic_value);
-                }
-
-                template <typename Callable>
-                auto apply(Callable&& cb) const
-                {
-                    return boost::apply_visitor(cb, m_generic_value);
-                }
-
-            private:
-                generic_variant_t get_init_value() const;
-                void copy_value(const generic_instance_value& other);
-
-            private:
-                const ast::type& m_type;
-                generic_instance_value_type m_value_type;
-                generic_variant_t m_generic_value;
-            };
-
             using instance_value_t = boost::variant<bool, int, double, std::string, generic_instance_value>;
-
-            // Wrapper that helps with forward declaration. generic stores it as unique_ptr. Thanks to that it can be chained -> generics of generics and so on.
-            struct instance_value_wrapper
-            {
-                instance_value_t val;
-            };
         }
     }
-}
-
-namespace std
-{
-    std::ostream &operator<<(std::ostream &out, const cmsl::exec::inst::generic_instance_value &);
 }
