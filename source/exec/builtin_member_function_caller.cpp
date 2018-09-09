55fbbdc906ddd072c87e67040cf72587f197b8fc
stryku <stryku2393@gmail.com>
2018-09-09 14:06:03 +0200

    moved fundamental push_back and version_ctor calls to ist own functions

--- source/exec/builtin_member_function_caller.cpp
+++ source/exec/builtin_member_function_caller.cpp
@@ -1,8 +1,11 @@
+#include <vector>
 #include "exec/builtin_member_function_caller.hpp"
 
 #include "ast/fundamental_function.hpp"
 #include "exec/builtin_function_visitors.hpp"
 #include "exec/instance/instances_holder.hpp"
+#include "instance/instance.hpp"
+#include "../ast/type.hpp"
 
 namespace cmsl
 {
@@ -38,29 +41,41 @@ namespace cmsl
                     }
                     case ff_kind_t::push_back:
                     {
-                        auto& val = class_instance->get_value_ref();
-                        const auto param = parameters[0];
-                        auto visitor = push_back_visitor{ *param };
-                        visitor.visit(val);
-                        return m_instances.create(true); // todo handle void return type. this true is to return any instance
+                        return call_push_back(class_instance, std::move(parameters));
                     }
                     case ff_kind_t::version_ctor:
                     {
-                        const auto major = parameters[0];
-                        const auto minor = parameters[1];
-                        const auto patch = parameters[2];
-                        const auto tweak = parameters[3];
-
-                        inst::instance_members_t members;
-                        members.emplace("major", major->copy());
-                        members.emplace("minor", minor->copy());
-                        members.emplace("patch", patch->copy());
-                        members.emplace("tweak", tweak->copy());
-
-                        return m_instances.create(class_instance->get_type(), std::move(members));
+                        return call_version_ctor(class_instance->get_type(), std::move(parameters));
                     }
                 }
             }
+        }
+
+        cmsl::exec::inst::instance *
+        cmsl::exec::builtin_member_function_caller::call_version_ctor(const ast::type &type, std::vector<inst::instance *> parameters)
+        {
+            const auto major = parameters[0];
+            const auto minor = parameters[1];
+            const auto patch = parameters[2];
+            const auto tweak = parameters[3];
+
+            inst::instance_members_t members;
+            members.emplace("major", major->copy());
+            members.emplace("minor", minor->copy());
+            members.emplace("patch", patch->copy());
+            members.emplace("tweak", tweak->copy());
+
+            return m_instances.create(type, std::move(members));
+        }
+
+        inst::instance *
+        cmsl::exec::builtin_member_function_caller::call_push_back(inst::instance *class_instance, std::vector<inst::instance *> parameters)
+        {
+            auto& val = class_instance->get_value_ref();
+            const auto param = parameters[0];
+            auto visitor = push_back_visitor{ *param };
+            visitor.visit(val);
+            return m_instances.create(true); // todo handle void return type. this true is to return any instance
         }
     }
 }
--- source/exec/builtin_member_function_caller.hpp
+++ source/exec/builtin_member_function_caller.hpp
@@ -6,6 +6,11 @@
 
 namespace cmsl
 {
+    namespace ast
+    {
+        class type;
+    }
+
     namespace exec
     {
         namespace inst
@@ -23,6 +28,10 @@ namespace cmsl
             inst::instance* call(inst::instance* class_instance,
                                  cmsl::string_view fun_name,
                                  std::vector<inst::instance*> parameters);
+
+        private:
+            inst::instance* call_push_back(inst::instance* class_instance, std::vector<inst::instance*> parameters);
+            inst::instance* call_version_ctor(const ast::type& type, std::vector<inst::instance*> parameters);
 
         private:
             inst::instances_holder& m_instances;
