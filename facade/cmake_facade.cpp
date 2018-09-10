2fa080e6902251799c7b929e645e32537ad117b9
stryku <stryku2393@gmail.com>
2018-09-10 19:56:46 +0200

    gather cmake version from cmake lib

--- facade/cmake_facade.cpp
+++ facade/cmake_facade.cpp
@@ -1,1 +1,19 @@
 #include "cmake_facade.hpp"
+
+#include <cmVersion.h>
+
+namespace cmsl
+{
+    namespace facade
+    {
+        cmake_facade::version cmake_facade::get_cmake_version() const
+        {
+            return {
+                    cmVersion::GetMajorVersion(),
+                    cmVersion::GetMinorVersion(),
+                    cmVersion::GetPatchVersion(),
+                    cmVersion::GetTweakVersion()
+            };
+        }
+    }
+}
