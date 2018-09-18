41b79fceae9f2252c9d14b05e2a1e556bf547999
stryku <stryku2393@gmail.com>
2018-09-18 19:33:17 +0200

    introduce ExpressionEvaluationTest in its own file

---
+++ test/exec/eval/ExpressionEvaluationTest.hpp
@@ -0,0 +1,48 @@
+#include <gmock/gmock.h>
+
+#include "ast/builtin_ast_context.hpp"
+#include "exec/infix/infix_evaluation_context.hpp"
+#include "exec/execution_context.hpp"
+#include "exec/instance/instances_holder.hpp"
+#include "exec/context_provider.hpp"
+
+namespace cmsl
+{
+    namespace test
+    {
+        namespace exec
+        {
+            namespace infix
+            {
+                class context_provider : public cmsl::exec::context_provider
+                {
+                public:
+                    const ast::ast_context& get_ast_ctx() const
+                    {
+                        return m_ast_ctx;
+                    }
+
+                    cmsl::exec::execution_context& get_exec_ctx()
+                    {
+                        return m_exec_ctx;
+                    }
+
+                private:
+                    cmsl::ast::builtin_ast_context m_ast_ctx;
+                    cmsl::exec::execution_context m_exec_ctx;
+                };
+
+                class ExpressionEvaluationTest : public testing::Test
+                {
+                protected:
+                    context_provider m_ctx_provider;
+                    cmsl::exec::inst::instances_holder m_instances{ m_ctx_provider };
+                    cmsl::exec::infix::infix_evaluation_context m_eval_ctx{
+                            m_ctx_provider,
+                            m_instances
+                    };
+                };
+            }
+        }
+    }
+}
