4987f0880dc64a1bea8db2ea270792a3f08d0aba
stryku <stryku2393@gmail.com>
2018-09-23 14:45:53 +0200

    introduce member_function_call_expression

---
+++ source/exec/infix/call_expression.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "exec/infix/infix_expression.hpp"
+
+#include "lexer/token/token.hpp"
+
+namespace cmsl
+{
+    namespace exec
+    {
+        class function_caller;
+
+        namespace infix
+        {
+            class call_expression : public infix_expression
+            {
+            public:
+                using params_t = std::vector<std::unique_ptr<infix_expression>>;
+
+                explicit call_expression(function_caller& fun_caller, lexer::token::token name, params_t parameter_expressions);
+
+            protected:
+                function_caller& get_caller() const;
+                cmsl::string_view get_name() const;
+                std::vector<inst::instance*> evaluate_params(infix_evaluation_context& ctx) const;
+
+            private:
+                function_caller& m_fun_caller;
+                cmsl::lexer::token::token m_name;
+                params_t m_parameter_expressions;
+            };
+        }
+    }
+}
--- test/exec/mock/function_caller_mock.hpp
+++ test/exec/mock/function_caller_mock.hpp
@@ -12,8 +12,12 @@ namespace cmsl
         {
             class function_caller_mock : public cmsl::exec::function_caller
             {
+            private:
+                using inst_t = cmsl::exec::inst::instance;
+
             public:
-                MOCK_METHOD2(call, cmsl::exec::inst::instance*(const cmsl::ast::function&, const std::vector<cmsl::exec::inst::instance*>&));
+                MOCK_METHOD2(call, inst_t*(const cmsl::ast::function&, const std::vector<inst_t*>&));
+                MOCK_METHOD3(call_member, inst_t*(inst_t&, const cmsl::ast::function&, const std::vector<inst_t*>&));
             };
         }
     }
