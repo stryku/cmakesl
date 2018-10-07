cff67e5c645aca09ca9547e759b17d592c957794
stryku <stryku2393@gmail.com>
2018-10-07 16:00:32 +0200

    added BuiltinAstContext.ProjectType_AddExecutableSignature test

--- test/ast/project_type_test.cpp
+++ test/ast/project_type_test.cpp
@@ -36,6 +36,20 @@ namespace cmsl
                 const auto string_type = m_ctx.find_type("string");
                 EXPECT_THAT(params_declaration[0].param_type, Eq(string_type));
             }
+
+            TEST_F(BuiltinAstContext, ProjectType_AddExecutableSignature)
+            {
+                const auto project_type = m_ctx.find_type("project");
+                const auto add_executable = project_type->get_function("add_executable");
+                ASSERT_THAT(add_executable, NotNull());
+
+                const auto params_declaration = add_executable->get_params_declarations();
+
+                EXPECT_THAT(params_declaration.size(), Eq(1u));
+
+                const auto source_list_type = m_ctx.find_type("list<string>");
+                EXPECT_THAT(params_declaration[0].param_type, Eq(source_list_type));
+            }
         }
     }
 }
