0cf849120b8ff15ec06dc18e8e32b27d5c00219f
stryku <stryku2393@gmail.com>
2018-10-09 21:58:07 +0200

    introduce cmake_facade_mock

---
+++ test/exec/mock/cmake_facade_mock.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "cmake_facade.hpp"
+
+#include <gmock/gmock.h>
+
+namespace cmsl
+{
+    namespace exec
+    {
+        namespace test
+        {
+            class function_caller_mock : public facade::cmake_facade
+            {
+            public:
+                MOCK_CONST_METHOD0(get_cmake_version, version());
+            };
+        }
+    }
+}
