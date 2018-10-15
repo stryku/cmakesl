d856809669959fa5ffc1f5db867ff4a5d3432426
stryku <stryku2393@gmail.com>
2018-10-15 20:33:53 +0200

    added cmake_minimum_version tests

---
+++ source/exec/builtin/cmake_minimum_required.cpp
@@ -0,0 +1,20 @@
+#include "exec/builtin/cmake_minimum_required.hpp"
+
+namespace cmsl
+{
+    namespace exec
+    {
+        namespace builtin
+        {
+            cmake_minimum_required::cmake_minimum_required(facade::cmake_facade &cmake_facade)
+            {
+
+            }
+
+            void cmake_minimum_required::call(const facade::cmake_facade::version &version)
+            {
+
+            }
+        }
+    }
+}
