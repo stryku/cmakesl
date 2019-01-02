663015fcf5496cc5ea2932c9263210f39b8cd50c
stryku <stryku2393@gmail.com>
2019-01-02 21:12:42 +0100

    Added basic sema_builder implementation

--- source/sema/sema_builder.cpp
+++ source/sema/sema_builder.cpp
@@ -1,1 +1,16 @@
-#include "sema_builder.hpp"
+#include "sema/sema_builder.hpp"
+
+namespace cmsl
+{
+    namespace sema
+    {
+        sema_builder::sema_builder(errors::errors_observer &errs)
+            : m_errs{ errs }
+        {}
+
+        std::unique_ptr<sema_node> sema_builder::build(const ast::ast_node &ast_tree)
+        {
+            return std::unique_ptr<sema_node>();
+        }
+    }
+}
