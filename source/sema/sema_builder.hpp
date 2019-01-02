6d20ed32f75ed12a121b967a162647cf32cfd3a4
stryku <stryku2393@gmail.com>
2019-01-02 21:12:42 +0100

    Added basic sema_builder implementation

--- source/sema/sema_builder.hpp
+++ source/sema/sema_builder.hpp
@@ -1,12 +1,34 @@
 #pragma once
 
+#include "ast/ast_node_visitor.hpp"
+
+#include <memory>
+
 namespace cmsl
 {
+    namespace ast
+    {
+        class ast_node;
+    }
+
+    namespace errors
+    {
+        class errors_observer;
+    }
+
     namespace sema
     {
+        class sema_node;
+
         class sema_builder
         {
+        public:
+            explicit sema_builder(errors::errors_observer& errs);
 
+            std::unique_ptr<sema_node> build(const ast::ast_node& ast_tree);
+
+        private:
+            errors::errors_observer& m_errs;
         };
     }
 }
