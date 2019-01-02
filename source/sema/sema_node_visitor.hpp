2d5ec8f7d7ddc630d47629b8696edf39d6e3af20
stryku <stryku2393@gmail.com>
2019-01-02 21:04:25 +0100

    Added SemaBuilderAstVisitorTest.Visit int_value_node and variable_declaration_node tests

--- source/sema/sema_node_visitor.hpp
+++ source/sema/sema_node_visitor.hpp
@@ -5,6 +5,7 @@ namespace cmsl
     namespace sema
     {
         class variable_declaration_node;
+        class int_value_node;
 
         class sema_node_visitor
         {
@@ -12,6 +13,7 @@ namespace cmsl
             virtual ~sema_node_visitor() = default;
 
             virtual void visit(const variable_declaration_node& node) = 0;
+            virtual void visit(const int_value_node& node) = 0;
         };
     }
 }
