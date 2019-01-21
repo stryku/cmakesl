ae805304a9faa4d510628d6785b9d0ae4f5db4fb
stryku <stryku2393@gmail.com>
2019-01-21 21:30:03 +0100

    Introduced basic builtin types operations

---
+++ source/exec/builtin_types_operations.hpp
@@ -0,0 +1,101 @@
+#pragma once
+
+#include "common/assert.hpp"
+#include "exec/instance/instance_value.hpp"
+
+#include <boost/optional.hpp>
+
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+namespace cmsl
+{
+    namespace exec
+    {
+        template <typename ValueType>
+        class value_operations_implementation
+        {
+        public:
+            using parameters_t = std::vector<inst::instance_value_t>;
+
+            boost::optional<inst::instance_value_t> execute_operation(std::string name, inst::instance_value_t& val, const parameters_t& params)
+            {
+                auto found = m_operations.find(name);
+                if(found == std::cend(m_operations))
+                {
+                    CMSL_UNREACHABLE("Evaluating unsupported operation");
+                    return {};
+                }
+
+                const auto& operation = found->second;
+                auto& value = boost::get<ValueType>(val);
+                return operation(value, params);
+            }
+
+        protected:
+            using operation_t = std::function<inst::instance_value_t(ValueType& val, const parameters_t&)>;
+
+            void add_operation(std::string name, operation_t op)
+            {
+                m_operations[name] = op;
+            }
+
+            static const ValueType& get_rhs(const parameters_t& params)
+            {
+                return boost::get<ValueType>(params[0]);
+            }
+
+        private:
+            std::unordered_map<std::string, operation_t> m_operations;
+        };
+
+        class bool_operations : public value_operations_implementation<bool>
+        {
+        public:
+            explicit bool_operations()
+            {
+                add_operation("&&", [](bool& lhs, const parameters_t& params)
+                {
+                    return lhs && get_rhs(params);
+                });
+                add_operation("||", [](bool& lhs, const parameters_t& params)
+                {
+                    return lhs || get_rhs(params);
+                });
+                add_operation("to_string", [](bool& lhs, const parameters_t&)
+                {
+                    return lhs ? "true" : "false";
+                });
+            }
+        };
+
+        class int_operations : public value_operations_implementation<inst::int_t>
+        {
+        public:
+            explicit int_operations()
+            {
+                add_operation("&&", [](inst::int_t& lhs, const parameters_t& params)
+                {
+                    return lhs && get_rhs(params);
+                });
+                add_operation("||", [](inst::int_t& lhs, const parameters_t& params)
+                {
+                    return lhs || get_rhs(params);
+                });
+                add_operation("+", [](inst::int_t& lhs, const parameters_t& params)
+                {
+                    return lhs + get_rhs(params);
+                });
+                add_operation("-", [](inst::int_t& lhs, const parameters_t& params)
+                {
+                    return lhs - get_rhs(params);
+                });
+                add_operation("to_string", [](inst::int_t& lhs, const parameters_t&)
+                {
+                    return std::to_string(lhs);
+                });
+            }
+        };
+    }
+}
