a051213ea0cda4281aab4984d1e95a754fada0d1
stryku <stryku2393@gmail.com>
2019-01-27 12:00:24 +0100

    Introduced type, function and context factories

---
+++ source/sema/factories.cpp
@@ -0,0 +1,24 @@
+#include "sema/factories.hpp"
+
+namespace cmsl
+{
+    namespace sema
+    {
+        sema_function_impl&
+        sema_function_factory::create(const sema_context_interface &ctx, const sema_type &return_type, function_signature s)
+        {
+            return create_impl(ctx, return_type, std::move(s));
+        }
+
+        const sema_type&
+        sema_type_factory::create(const sema_context_interface &ctx, lexer::token::token name, std::vector<member_info> members)
+        {
+            return create_impl(ctx, name, std::move(members));
+        }
+
+        const sema_context_interface& sema_context_factory::create(const sema_context_interface *parent)
+        {
+            return create_impl(parent);
+        }
+    }
+}
