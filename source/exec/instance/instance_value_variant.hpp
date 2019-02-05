a8d41849606572215df23f5cfc648214b9365c60
stryku <stryku2393@gmail.com>
2019-02-05 13:30:37 +0100

    Moved instance_value_variant's methods to cpp file

--- source/exec/instance/instance_value_variant.hpp
+++ source/exec/instance/instance_value_variant.hpp
@@ -42,216 +42,76 @@ namespace cmsl
                     generic
                 };
 
-                instance_value_variant(const instance_value_variant& other)
-                {
-                    reassign(other.m_value, other.m_which);
-                }
+                instance_value_variant(const instance_value_variant& other);
 
-                instance_value_variant& operator=(const instance_value_variant& other)
-                {
-                    if(this != &other)
-                    {
-                        reassign(other.m_value, other.m_which);
-                    }
-
-                    return *this;
-                }
+                instance_value_variant& operator=(const instance_value_variant& other);
 
                 instance_value_variant(instance_value_variant&&) = default;
                 instance_value_variant& operator=(instance_value_variant&&)  = default;
 
-                explicit instance_value_variant()
-                        : instance_value_variant{ false }
-                {}
+                explicit instance_value_variant();
 
-                instance_value_variant(bool val)
-                {
-                    assign(val, which_type::bool_);
-                }
+                instance_value_variant(bool val);
 
-                instance_value_variant(int_t val)
-                {
-                    assign(val, which_type::int_);
-                }
+                instance_value_variant(int_t val);
 
-                instance_value_variant(double val)
-                {
-                    assign(val, which_type::double_);
-                }
+                instance_value_variant(double val);
 
                 // Prevent conversion from const char* to bool.
-                instance_value_variant(const char* value)
-                    : instance_value_variant{ std::string{ value } }
-                {}
+                instance_value_variant(const char* value);
 
-                instance_value_variant(std::string val)
-                {
-                    assign(std::move(val), which_type::string);
-                }
+                instance_value_variant(std::string val);
 
-                instance_value_variant(generic_instance_value val)
-                {
-                    assign(std::move(val), which_type::generic);
-                }
+                instance_value_variant(generic_instance_value val);
+                ~instance_value_variant();
 
-                ~instance_value_variant()
-                {
-                    destruct();
-                }
+                which_type which() const;
 
-                which_type which() const
-                {
-                    return m_which;
-                }
+                bool get_bool() const;
 
-                bool get_bool() const
-                {
-                    return m_value.m_bool;
-                }
+                void set_bool(bool value);
 
-                void set_bool(bool value)
-                {
-                    reassign(value, which_type::bool_);
-                }
+                int_t get_int() const;
+                void set_int(int_t value);
 
-                int_t get_int() const
-                {
-                    return m_value.m_int;
-                }
+                double get_double() const;
 
-                void set_int(int_t value)
-                {
-                    reassign(value, which_type::int_);
-                }
+                void set_double(double value);
 
-                double get_double() const
-                {
-                    return m_value.m_double;
-                }
+                const std::string& get_string_cref() const;
 
-                void set_double(double value)
-                {
-                    reassign(value, which_type::double_);
-                }
+                void set_string(std::string value);
 
-                const std::string& get_string_cref() const
-                {
-                    return m_value.m_string;
-                }
+                const generic_instance_value& get_generic_cref() const;
 
-                void set_string(std::string value)
-                {
-                    reassign(std::move(value), which_type::string);
-                }
-
-                const generic_instance_value& get_generic_cref() const
-                {
-                    return m_value.m_generic;
-                }
-
-                void set_generic(generic_instance_value value)
-                {
-                    reassign(std::move(value), which_type::generic);
-                }
+                void set_generic(generic_instance_value value);
 
             private:
                 template <typename T>
-                void reassign(T&& val, which_type w)
-                {
-                    destruct();
-                    assign(std::forward<T>(val), w);
-                }
+                void reassign(T&& val, which_type w);
 
-                void assign(const value& other, which_type w)
-                {
-                    switch (w)
-                    {
-                        case which_type::bool_:
-                        {
-                            assign(other.m_bool, w);
-                        }break;
-                        case which_type::int_:
-                        {
-                            assign(other.m_int, w);
-                        }break;
-                        case which_type::double_:
-                        {
-                            assign(other.m_double, w);
-                        }break;
-                        case which_type::string:
-                        {
-                            assign(other.m_string, w);
-                        }break;
-                        case which_type::generic:
-                        {
-                            assign(other.m_generic, w);
-                        }break;
-                    }
-                }
+                void assign(const value& other, which_type w);
 
                 template <typename T>
-                void assign(T&& val, which_type w)
-                {
-                    m_which  = w;
-                    construct(std::forward<T>(val));
-                }
+                void assign(T&& val, which_type w);
 
-                void construct(bool value)
-                {
-                    m_value.m_bool = value;
-                }
+                void construct(bool value);
 
-                void construct(int_t value)
-                {
-                    m_value.m_int = value;
-                }
+                void construct(int_t value);
 
-                void construct(double value)
-                {
-                    m_value.m_double = value;
-                }
+                void construct(double value);
 
-                void construct(std::string value)
-                {
-                    construct(m_value.m_string, std::move(value));
-                }
+                void construct(std::string value);
 
-                void construct(generic_instance_value value)
-                {
-                    construct(m_value.m_generic, std::move(value));
-                }
+                void construct(generic_instance_value value);
 
                 template <typename Value>
-                void construct(Value& destination_ptr, Value&& value)
-                {
-                    new (&destination_ptr) Value{ std::move(value) };
-                }
+                void construct(Value& destination_ptr, Value&& value);
 
-                void destruct()
-                {
-                    switch (m_which)
-                    {
-                        case which_type::bool_:
-                        case which_type::int_:
-                        case which_type::double_:
-                            // Primitives, do nothing.
-                            break;
-                        case which_type::string:
-                        {
-                            call_dtor(m_value.m_string);
-                        } break;
-                        case which_type::generic:
-                        {
-                            call_dtor(m_value.m_generic);
-                        } break;
-                    }
-                }
+                void destruct();
 
                 template <typename T>
-                void call_dtor(T& val)
-                {
-                    val.~T();
-                }
+                void call_dtor(T& val);
 
             private:
                 which_type m_which;
