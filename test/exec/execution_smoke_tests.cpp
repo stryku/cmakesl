6f8b7d58ff3834f2cfe346e6c62686bedf4bf0c4
stryku <stryku2393@gmail.com>
2019-02-09 19:38:09 +0100

    Added SourceExecutorTest.Class test

--- test/exec/execution_smoke_tests.cpp
+++ test/exec/execution_smoke_tests.cpp
@@ -52,6 +52,28 @@ namespace cmsl
                 const auto result = executor.execute2(source);
                 EXPECT_THAT(result, Eq(-42));
             }
+
+            TEST(SourceExecutorTest, Class)
+            {
+                const auto source =
+                        "class foo"
+                        "{"
+                        "    int value;"
+                        "    int get_value() { return value; }"
+                        "};"
+                        ""
+                        "int main()"
+                        "{"
+                        "    foo bar;"
+                        "    bar.value = 42;"
+                        "    return foo.get_value();"
+                        "}";
+
+                cmake_facade_mock facade;
+                source_executor executor{ facade };
+                const auto result = executor.execute2(source);
+                EXPECT_THAT(result, Eq(42));
+            }
         }
     }
 }
