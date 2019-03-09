ae5124e2481219d1344578a8f0eec93cf175984f
stryku <stryku2393@gmail.com>
2019-03-09 21:15:37 +0100

    Implemented string operators

--- source/exec/instance/instance_value_variant.hpp
+++ source/exec/instance/instance_value_variant.hpp
@@ -66,18 +66,16 @@ namespace cmsl
                 which_type which() const;
 
                 bool get_bool() const;
-
                 void set_bool(bool value);
 
                 int_t get_int() const;
                 void set_int(int_t value);
 
                 double get_double() const;
-
                 void set_double(double value);
 
                 const std::string& get_string_cref() const;
-
+                std::string& get_string_ref();
                 void set_string(std::string value);
 
                 const generic_instance_value& get_generic_cref() const;
--- test/exec/string_type_smoke_tests.cpp
+++ test/exec/string_type_smoke_tests.cpp
@@ -53,6 +53,137 @@ namespace cmsl
                 const auto result = m_executor.execute2(source);
                 EXPECT_THAT(result, Eq(1));
             }
+
+            TEST_F(StringTypeSmokeTest, ExplicitConstructorWithString)
+            {
+                const auto source =
+                        "int main()\n"
+                        "{\n"
+                        "    string s = string(\"foo\");\n"
+                        "    string s2 = string(s);"
+                        "    return int(s2.size() == 3 && s2.empty() == false);\n"
+                        "}";
+                const auto result = m_executor.execute2(source);
+                EXPECT_THAT(result, Eq(1));
+            }
+
+            TEST_F(StringTypeSmokeTest, OperatorEqual)
+            {
+                const auto source =
+                        "int main()\n"
+                        "{\n"
+                        "    string s = \"foo\";\n"
+                        "    string s2 = s;"
+                        "    return int(s2.size() == 3 && s2.empty() == false);\n"
+                        "}";
+                const auto result = m_executor.execute2(source);
+                EXPECT_THAT(result, Eq(1));
+            }
+
+            TEST_F(StringTypeSmokeTest, OperatorEqualEqual)
+            {
+                const auto source =
+                        "int main()\n"
+                        "{\n"
+                        "    string s = \"foo\";\n"
+                        "    string s2 = s;"
+                        "    return int(s == s2);\n"
+                        "}";
+                const auto result = m_executor.execute2(source);
+                EXPECT_THAT(result, Eq(1));
+            }
+
+            TEST_F(StringTypeSmokeTest, OperatorNotEqual)
+            {
+                const auto source =
+                        "int main()\n"
+                        "{\n"
+                        "    string s = \"foo\";\n"
+                        "    string s2 = \"fooo\";\n"
+                        "    return int(s != s2);\n"
+                        "}";
+                const auto result = m_executor.execute2(source);
+                EXPECT_THAT(result, Eq(1));
+            }
+
+            TEST_F(StringTypeSmokeTest, OperatorLess)
+            {
+                const auto source =
+                        "int main()\n"
+                        "{\n"
+                        "    string s = \"bar\";\n"
+                        "    string s2 = \"foo\";\n"
+                        "    return int(s < s2);\n"
+                        "}";
+                const auto result = m_executor.execute2(source);
+                EXPECT_THAT(result, Eq(1));
+            }
+
+            TEST_F(StringTypeSmokeTest, OperatorLessEqual)
+            {
+                const auto source =
+                        "int main()\n"
+                        "{\n"
+                        "    string s = \"bar\";\n"
+                        "    string s2 = \"bar\";\n"
+                        "    return int(s <= s2);\n"
+                        "}";
+                const auto result = m_executor.execute2(source);
+                EXPECT_THAT(result, Eq(1));
+            }
+
+            TEST_F(StringTypeSmokeTest, OperatorGreater)
+            {
+                const auto source =
+                        "int main()\n"
+                        "{\n"
+                        "    string s = \"foo\";\n"
+                        "    string s2 = \"bar\";\n"
+                        "    return int(s > s2);\n"
+                        "}";
+                const auto result = m_executor.execute2(source);
+                EXPECT_THAT(result, Eq(1));
+            }
+
+            TEST_F(StringTypeSmokeTest, OperatorGreaterEqual)
+            {
+                const auto source =
+                        "int main()\n"
+                        "{\n"
+                        "    string s = \"bar\";\n"
+                        "    string s2 = \"bar\";\n"
+                        "    return int(s >= s2);\n"
+                        "}";
+                const auto result = m_executor.execute2(source);
+                EXPECT_THAT(result, Eq(1));
+            }
+
+            TEST_F(StringTypeSmokeTest, OperatorPlus)
+            {
+                const auto source =
+                        "int main()\n"
+                        "{\n"
+                        "    string s = \"foo +\";\n"
+                        "    string s2 = \" bar\";\n"
+                        "    string s3 = s + s2;\n"
+                        "    return int(s3 == \"foo + bar\");\n"
+                        "}";
+                const auto result = m_executor.execute2(source);
+                EXPECT_THAT(result, Eq(1));
+            }
+
+            TEST_F(StringTypeSmokeTest, OperatorPlusEqual)
+            {
+                const auto source =
+                        "int main()\n"
+                        "{\n"
+                        "    string s = \"foo +\";\n"
+                        "    s += \" bar\";\n"
+                        "    return int(s == \"foo + bar\");\n"
+                        "}";
+                const auto result = m_executor.execute2(source);
+                EXPECT_THAT(result, Eq(1));
+            }
         }
     }
 }
