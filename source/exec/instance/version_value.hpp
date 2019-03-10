35694dae8574175886671df785d99ef723911d3a
stryku <stryku2393@gmail.com>
2019-03-10 13:04:37 +0100

    Introduced version_value class

---
+++ source/exec/instance/version_value.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+
+namespace cmsl
+{
+    namespace exec
+    {
+        namespace inst
+        {
+            class version_value
+            {
+            public:
+                explicit version_value(unsigned major, unsigned minor = 0u, unsigned patch = 0u, unsigned tweak = 0u);
+
+                std::string to_string() const;
+
+                bool operator<(const version_value& rhs) const;
+
+            private:
+                unsigned m_major{ 0u };
+                unsigned m_minor{ 0u };
+                unsigned m_patch{ 0u };
+                unsigned m_tweak{ 0u };
+            };
+        }
+    }
+}
