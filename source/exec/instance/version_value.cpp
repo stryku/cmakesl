37d48e6bd7489bff49663657a187d8fb442187f5
stryku <stryku2393@gmail.com>
2019-03-10 13:04:37 +0100

    Introduced version_value class

---
+++ source/exec/instance/version_value.cpp
@@ -0,0 +1,38 @@
+#include "exec/instance/version_value.hpp"
+#include "version_value.hpp"
+
+#include <algorithm>
+
+namespace cmsl
+{
+    namespace exec
+    {
+        namespace inst
+        {
+            version_value::version_value(unsigned major, unsigned minor, unsigned patch, unsigned tweak)
+                : m_major{ major }
+                , m_minor{ minor }
+                , m_patch{ patch }
+                , m_tweak{ tweak }
+            {}
+
+            std::string version_value::to_string() const
+            {
+                // Todo: Skip trailing zeroes. E.g. 1.2.0.0, should be "1.2".
+                return std::to_string(m_major) + '.'
+                       + std::to_string(m_minor) + '.'
+                       + std::to_string(m_patch) + '.'
+                       + std::to_string(m_tweak);
+            }
+
+            bool version_value::operator<(const version_value &rhs) const
+            {
+                const auto this_parts = { m_major, m_minor, m_patch, m_tweak };
+                const auto rhs_parts = { rhs.m_major, rhs.m_minor, rhs.m_patch, rhs.m_tweak };
+
+                return std::lexicographical_compare(std::cbegin(this_parts), std::cend(this_parts),
+                                                    std::cbegin(rhs_parts), std::cend(rhs_parts));
+            }
+        }
+    }
+}
