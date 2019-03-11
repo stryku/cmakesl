500b5df55d1a9e64238ae9fc38ac6db3fb934cee
stryku <stryku2393@gmail.com>
2019-03-11 21:40:19 +0100

    Store necessary tokens in type_reference

--- source/ast/type_reference.hpp
+++ source/ast/type_reference.hpp
@@ -6,15 +6,22 @@ namespace cmsl
 {
     namespace ast
     {
+        // A structure that holds tokens that represent given type. E.g.:
+        // double => tokens vector is: {double}
+        // map<int, list<string>> => tokens vector is: {map, int, list, string}
         struct type_reference
         {
-            lexer::token::token begin;
-            lexer::token::token end;
+            //lexer::token::token begin;
+            //lexer::token::token end;
+            std::vector<lexer::token::token> tokens;
+
+            // We need to keep it to easily format errors in case of generic types, which ends with '>'.
+            lexer::token::token end_token;
 
             cmsl::string_view to_string() const
             {
-                const auto b = begin.str().data();
-                return cmsl::string_view{ b, static_cast<size_t>(end.str().cend() - b)};
+                const auto b = tokens.front().str().data();
+                return cmsl::string_view{ b, static_cast<size_t>(end_token.str().cend() - b)};
             }
         };
     }
