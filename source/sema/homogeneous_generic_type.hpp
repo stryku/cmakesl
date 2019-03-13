3273a5f38c85bd75a362aae1934e6f431b797a43
stryku <stryku2393@gmail.com>
2019-03-13 22:04:03 +0100

    Introduced homogeneous_generic_type

---
+++ source/sema/homogeneous_generic_type.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "sema/sema_type.hpp"
+
+namespace cmsl
+{
+    namespace sema
+    {
+        class homogeneous_generic_type : public sema_type
+        {
+        public:
+            explicit homogeneous_generic_type(const sema_context_interface& ctx, ast::type_name_reference name, const sema_type& value_type);
+
+            const sema_type& value_type() const;
+
+        private:
+            const sema_type& m_value_type;
+        };
+    }
+}
