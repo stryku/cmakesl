b687d4915e62c29b1d05aa29adf6ec93ad26f2b6
stryku <stryku2393@gmail.com>
2019-03-25 21:39:24 +0100

    Moved parser_utils to its own files

--- source/ast/parser2.hpp
+++ source/ast/parser2.hpp
@@ -2,9 +2,7 @@
 
 #include "ast/type_representation.hpp"
 #include "ast/parameter_declaration.hpp"
-#include "lexer/token/token.hpp"
-
-#include <boost/optional.hpp>
+#include "ast/parser_utils.hpp"
 
 #include <memory>
 
@@ -20,36 +18,6 @@ namespace cmsl
         class ast_node;
         class conditional_node;
         class block_node;
-
-        class parser_utils
-        {
-        protected:
-            using token_container_t = lexer::token::token_container_t;
-            using token_it = token_container_t::const_iterator;
-            using token_type_t = lexer::token::token_type;
-            using token_t = lexer::token::token;
-
-            explicit parser_utils(errors::errors_observer& err_observer, token_it current, token_it end);
-
-            bool expect_not_at_end();
-            bool is_at_end() const;
-
-            token_type_t peek(size_t n = 1u) const;
-            boost::optional<token_t> eat(boost::optional<token_type_t> type = {});
-
-            const token_t& current() const;
-            token_type_t curr_type() const;
-            bool next_is(token_type_t token_type) const;
-            bool current_is(token_type_t token_type) const;
-
-            void raise_error(lexer::token::token token, std::string message);
-
-
-        private:
-            errors::errors_observer& m_err_observer;
-            token_it m_current;
-            token_it m_end;
-        };
 
         // Todo: rename methods to parse_*
         class parser2 : public parser_utils
@@ -81,7 +49,6 @@ namespace cmsl
             boost::optional<token_t> eat_generic_type_token();
             boost::optional<token_t> eat_simple_type_token();
             boost::optional<token_t> eat_function_call_name();
-
 
             boost::optional<type_representation> generic_type();
             boost::optional<type_representation> simple_type();
