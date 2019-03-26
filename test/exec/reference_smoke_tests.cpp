6ab5e7112a867d7b70c7f4336270f04464a3d197
stryku <stryku2393@gmail.com>
2019-03-26 21:35:40 +0100

    Added UserType reference smoke test

--- test/exec/reference_smoke_tests.cpp
+++ test/exec/reference_smoke_tests.cpp
@@ -18,8 +18,7 @@ namespace cmsl
                 source_executor m_executor{m_facade};
             };
 
-            // Todo: rename
-            TEST_F(ReferenceTypeSmokeTest, WorksAsExpected)
+            TEST_F(ReferenceTypeSmokeTest, BuiltinType)
             {
                 const auto source =
                         "int foo(int& ref)\n"
@@ -55,6 +54,54 @@ namespace cmsl
                         "\n"
                         "    got_ref = 42;\n"
                         "    result = result && i == 42 && i_ref == 42 && got_ref == 42;\n"
+                        "\n"
+                        "    return int(result);\n"
+                        "}";
+                const auto result = m_executor.execute2(source);
+                EXPECT_THAT(result, Eq(1));
+            }
+
+            TEST_F(ReferenceTypeSmokeTest, UserType)
+            {
+                const auto source =
+                        "class user_class\n"
+                        "{\n"
+                        "    int v;\n"
+                        "};\n"
+                        "\n"
+                        "int foo(user_class& ref)\n"
+                        "{\n"
+                        "    ref.v = 42;\n"
+                        "    return 0;\n"
+                        "}\n"
+                        "\n"
+                        "user_class& set_and_get_ref(user_class& ref)\n"
+                        "{\n"
+                        "    ref.v = 24;\n"
+                        "    return ref;\n"
+                        "}\n"
+                        "\n"
+                        "int main()\n"
+                        "{\n"
+                        "    user_class c;\n"
+                        "    user_class& c_ref = c;\n"
+                        "    bool result = true;\n"
+                        "    result = result && c.v == 0 && c_ref.v == 0;\n"
+                        "\n"
+                        "    c.v = 42;\n"
+                        "    result = result && c.v == 42 && c_ref.v == 42;\n"
+                        "\n"
+                        "    c_ref.v = 24;\n"
+                        "    result = result && c.v == 24 && c_ref.v == 24;\n"
+                        "\n"
+                        "    foo(c_ref);\n"
+                        "    result = result && c.v == 42 && c_ref.v == 42;\n"
+                        "\n"
+                        "    user_class& got_ref = set_and_get_ref(c);\n"
+                        "    result = result && c.v == 24 && c_ref.v == 24 && got_ref.v == 24;\n"
+                        "\n"
+                        "    got_ref.v = 42;\n"
+                        "    result = result && c.v == 42 && c_ref.v == 42 && got_ref.v == 42;\n"
                         "\n"
                         "    return int(result);\n"
                         "}";
