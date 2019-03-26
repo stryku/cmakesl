387531a4689c01c151e7089b85db2839120ae80c
stryku <stryku2393@gmail.com>
2019-03-26 22:19:00 +0100

    Introduced source_view

---
+++ source/common/source_view.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "common/string.hpp"
+
+namespace cmsl
+{
+    class source_view
+    {
+    public:
+        explicit source_view(cmsl::string_view source);
+        explicit source_view(cmsl::string_view source_path, cmsl::string_view source);
+
+        cmsl::string_view path() const;
+        cmsl::string_view source() const;
+
+        // Line numbers start from 1, not 0.
+        cmsl::string_view line(unsigned line_no) const;
+
+    private:
+        cmsl::string_view m_path;
+        cmsl::string_view m_source;
+    };
+}
