5c2832a41e941d3ee39cddb2b9f7853629bdeaec
stryku <stryku2393@gmail.com>
2019-03-31 17:59:49 +0200

    Introduced source_compiler and compiled_source

---
+++ source/exec/source_compiler.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+namespace cmsl
+{
+    class source_view;
+
+    namespace errors
+    {
+        class errors_observer;
+    }
+
+    namespace sema
+    {
+        class sema_type_factory;
+        class sema_function_factory;
+        class sema_context_factory;
+    }
+
+    namespace exec
+    {
+        class compiled_source;
+
+        class source_compiler
+        {
+        public:
+            explicit source_compiler(errors::errors_observer& errors_observer,
+                                     sema::sema_type_factory& type_factory,
+                                     sema::sema_function_factory& function_factory,
+                                     sema::sema_context_factory& context_factory);
+
+            compiled_source compile(source_view source);
+
+        private:
+            errors::errors_observer& m_errors_observer;
+            sema::sema_type_factory& m_type_factory;
+            sema::sema_function_factory& m_function_factory;
+            sema::sema_context_factory& m_context_factory;
+        };
+    }
+}
