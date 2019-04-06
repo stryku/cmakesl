378cdc1237acad7b3ce60347280dddc6db945a02
stryku <stryku2393@gmail.com>
2019-04-06 19:38:14 +0200

    Made builtin funcitons returning references

--- source/sema/sema_context.hpp
+++ source/sema/sema_context.hpp
@@ -44,6 +44,7 @@ namespace cmsl
             virtual void add_type(const sema_type& type) = 0;
             virtual const sema_type* find_type(const ast::type_representation& name) const = 0;
             virtual const sema_type* find_referenced_type(const ast::type_representation& name) const = 0;
+            virtual const sema_type* find_reference_for(const sema_type& type) const = 0;
             virtual const sema_type* find_type_in_this_scope(const ast::type_representation& name) const = 0;
             virtual function_lookup_result_t find_function(const lexer::token::token& name) const = 0;
             virtual single_scope_function_lookup_result_t find_function_in_this_scope(const lexer::token::token& name) const = 0;
@@ -59,6 +60,7 @@ namespace cmsl
             void add_type(const sema_type& type) override;
             const sema_type* find_type(const ast::type_representation& name) const override;
             const sema_type* find_referenced_type(const ast::type_representation& name) const override;
+            const sema_type* find_reference_for(const sema_type& type) const override;
             const sema_type* find_type_in_this_scope(const ast::type_representation& name) const override;
             function_lookup_result_t find_function(const lexer::token::token& name) const override;
             single_scope_function_lookup_result_t find_function_in_this_scope(const lexer::token::token& name) const override;
