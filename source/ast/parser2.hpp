9ebf282b371bfc996f79032ba9fbc110eac2d895
stryku <stryku2393@gmail.com>
2019-04-06 22:04:07 +0200

    Refactored operator parsing

--- source/ast/parser2.hpp
+++ source/ast/parser2.hpp
@@ -56,31 +56,9 @@ namespace cmsl
             bool function_declaration_starts() const;
             bool declaration_starts() const;
 
-            // It is ugly, but it is better to keep it this way for grammar purpose. Consider refactor, though.
-            bool is_current_operator_2() const;
-            bool is_current_operator_3() const;
-            bool is_current_operator_5() const;
-            bool is_current_operator_6() const;
-            bool is_current_operator_9() const;
-            bool is_current_operator_10() const;
-            bool is_current_operator_11() const;
-            bool is_current_operator_12() const;
-            bool is_current_operator_13() const;
-            bool is_current_operator_14() const;
-            bool is_current_operator_15() const;
-            bool is_current_operator_16() const;
-            std::unique_ptr<ast_node> operator_2();
-            std::unique_ptr<ast_node> operator_3();
-            std::unique_ptr<ast_node> operator_5();
-            std::unique_ptr<ast_node> operator_6();
-            std::unique_ptr<ast_node> operator_9();
-            std::unique_ptr<ast_node> operator_10();
-            std::unique_ptr<ast_node> operator_11();
-            std::unique_ptr<ast_node> operator_12();
-            std::unique_ptr<ast_node> operator_13();
-            std::unique_ptr<ast_node> operator_14();
-            std::unique_ptr<ast_node> operator_15();
-            std::unique_ptr<ast_node> operator_16();
+            static constexpr auto k_min_precedence{ 2u };
+            static constexpr auto k_max_precedence{ 16u };
+            std::unique_ptr<ast_node> parse_operator(unsigned precedence = k_max_precedence);
 
             std::unique_ptr<ast_node> fundamental_value();
             std::unique_ptr<ast_node> function_call();
