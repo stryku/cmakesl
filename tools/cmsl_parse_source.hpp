40dbdc36a86d4694836249af1f80aa830dec1f08
stryku <stryku2393@gmail.com>
2019-04-22 23:18:39 +0200

    Introduced examples for completion and indexing

--- tools/cmsl_parse_source.hpp
+++ tools/cmsl_parse_source.hpp
@@ -1,12 +1,18 @@
 #ifndef CMSL_PARSE_SOURCE_HPP
 #define CMSL_PARSE_SOURCE_HPP
 
+#ifdef __cplusplus
 extern "C"
 {
+#endif
+
 struct cmsl_parsed_source;
 
-cmsl_parsed_source *cmsl_parse_source(const char *source);
-void cmsl_destroy_parsed_source(cmsl_parsed_source *parsed_source);
+struct cmsl_parsed_source *cmsl_parse_source(const char *source);
+void cmsl_destroy_parsed_source(struct cmsl_parsed_source *parsed_source);
+
+#ifdef __cplusplus
 }
+#endif
 
 #endif //CMSL_PARSE_SOURCE_HPP
