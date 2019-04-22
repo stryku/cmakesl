fb70bb65fff44930d4e2dd89568c769109fbd65f
stryku <stryku2393@gmail.com>
2019-04-22 22:02:37 +0200

    Use std::variant in instance_value_variant

--- source/exec/instance/instance_value_variant.cpp
+++ source/exec/instance/instance_value_variant.cpp
@@ -6,97 +6,21 @@
 
 namespace cmsl::exec::inst
 {
-            instance_value_variant::instance_value_variant(instance_value_variant && other)
-            {
-                move_from(std::move(other));
-            }
-
-            instance_value_variant &instance_value_variant::operator=(instance_value_variant && other)
-            {
-                if(this != &other)
-                {
-                    move_from(std::move(other));
-                }
-
-                return *this;
-            }
-
-            void instance_value_variant::move_from(instance_value_variant&& moved)
-            {
-                switch (moved.m_which)
-                {
-                    case which_t::bool_:
-                    {
-                        reassign(moved.m_value.m_bool, which_t::bool_);
-                    }break;
-                    case which_t::int_:
-                    {
-                        reassign(moved.m_value.m_int, which_t::int_);
-                    }break;
-                    case which_t::double_:
-                    {
-                        reassign(moved.m_value.m_double, which_t::double_);
-                    }break;
-                    case which_t::string:
-                    {
-                        reassign(std::move(moved.m_value.m_string), which_t::string);
-                    }break;
-                    case which_t::version:
-                    {
-                        reassign(std::move(moved.m_value.m_version), which_t::version);
-                    }break;
-                    case which_t::list:
-                    {
-                        reassign(std::move(moved.m_value.m_list), which_t::list);
-                    }break;
-                    case which_t::project:
-                    {
-                        reassign(std::move(moved.m_value.m_project), which_t::project);
-                    }break;
-                    case which_t::library:
-                    {
-                        reassign(std::move(moved.m_value.m_library), which_t::library);
-                    }break;
-                    case which_t::executable:
-                    {
-                        reassign(std::move(moved.m_value.m_executable), which_t::executable);
-                    }break;
-                }
-            }
-
-            instance_value_variant::instance_value_variant(const instance_value_variant& other)
-            {
-                reassign(other.m_value, other.m_which);
-            }
-
-            instance_value_variant& instance_value_variant::operator=(const instance_value_variant& other)
-            {
-                if(this != &other)
-                {
-                    reassign(other.m_value, other.m_which);
-                }
-
-                return *this;
-            }
-
             instance_value_variant::instance_value_variant()
                     : instance_value_variant{ false }
             {}
 
             instance_value_variant::instance_value_variant(bool val)
-            {
-                assign(val, which_t::bool_);
-            }
+                : m_value{ val }
+            {}
 
             instance_value_variant::instance_value_variant(int_t val)
-            {
-                assign(val, which_t::int_);
-            }
+                    : m_value{ val }
+            {}
 
             instance_value_variant::instance_value_variant(double val)
-            {
-                assign(val, which_t::double_);
-            }
+                    : m_value{ val }
+            {}
 
             // Prevent conversion from const char* to bool.
             instance_value_variant::instance_value_variant(const char* value)
@@ -104,245 +28,88 @@ namespace cmsl::exec::inst
             {}
 
             instance_value_variant::instance_value_variant(std::string val)
-            {
-                assign(std::move(val), which_t::string);
-            }
+                    : m_value{ val }
+            {}
 
             instance_value_variant::instance_value_variant(version_value val)
-            {
-                assign(std::move(val), which_t::version);
-            }
+                    : m_value{ val }
+            {}
 
             instance_value_variant::instance_value_variant(list_value val)
-            {
-                assign(std::move(val), which_t::list);
-            }
+                    : m_value{ val }
+            {}
 
             instance_value_variant::instance_value_variant(project_value val)
-            {
-                assign(std::move(val), which_t::project);
-            }
+                    : m_value{ val }
+            {}
 
             instance_value_variant::instance_value_variant(library_value val)
-            {
-                assign(std::move(val), which_t::library);
-            }
+                    : m_value{ val }
+            {}
 
             instance_value_variant::instance_value_variant(executable_value val)
-            {
-                assign(std::move(val), which_t::executable);
-            }
+                    : m_value{ val }
+            {}
 
-            instance_value_variant::~instance_value_variant()
-            {
-                destruct();
-            }
+
 
             instance_value_variant::which_t instance_value_variant::which() const
             {
-                return m_which;
+                return static_cast<which_t>(m_value.index());
             }
 
             bool instance_value_variant::get_bool() const
             {
-                return m_value.m_bool;
+                return std::get<bool>(m_value);
             }
 
             void instance_value_variant::set_bool(bool value)
             {
-                reassign(value, which_t::bool_);
+                m_value = value;
             }
 
             int_t instance_value_variant::get_int() const
             {
-                return m_value.m_int;
+                return std::get<int_t>(m_value);
             }
 
             void instance_value_variant::set_int(int_t value)
             {
-                reassign(value, which_t::int_);
+                m_value = value;
             }
 
             double instance_value_variant::get_double() const
             {
-                return m_value.m_double;
+                return std::get<double>(m_value);
             }
 
             void instance_value_variant::set_double(double value)
             {
-                reassign(value, which_t::double_);
+                m_value = value;
             }
 
             const std::string& instance_value_variant::get_string_cref() const
             {
-                return m_value.m_string;
+                return std::get<std::string>(m_value);
             }
 
             std::string &instance_value_variant::get_string_ref()
             {
-                return m_value.m_string;
+                return std::get<std::string>(m_value);
             }
 
             void instance_value_variant::set_string(std::string value)
             {
-                reassign(std::move(value), which_t::string);
-            }
-
-            template <typename T>
-            void instance_value_variant::reassign(T&& val, which_t w)
-            {
-                destruct();
-                assign(std::forward<T>(val), w);
-            }
-
-            void instance_value_variant::assign(const value& other, which_t w)
-            {
-                switch (w)
-                {
-                    case which_t::bool_:
-                    {
-                        assign(other.m_bool, w);
-                    }break;
-                    case which_t::int_:
-                    {
-                        assign(other.m_int, w);
-                    }break;
-                    case which_t::double_:
-                    {
-                        assign(other.m_double, w);
-                    }break;
-                    case which_t::string:
-                    {
-                        assign(other.m_string, w);
-                    }break;
-                    case which_t::version:
-                    {
-                        assign(other.m_version, w);
-                    }break;
-                    case which_t::list:
-                    {
-                        list_value list_copy = other.m_list;
-                        assign(std::move(list_copy), w);
-                    }break;
-                    case which_t::project:
-                    {
-                        assign(other.m_project, w);
-                    }break;
-                    case which_t::library:
-                    {
-                        assign(other.m_library, w);
-                    }break;
-                    case which_t::executable:
-                    {
-                        assign(other.m_executable, w);
-                    }break;
-                }
-            }
-
-            template <typename T>
-            void instance_value_variant::assign(T&& val, which_t w)
-            {
-                m_which  = w;
-                construct(std::forward<T>(val));
-            }
-
-            void instance_value_variant::construct(bool value)
-            {
-                m_value.m_bool = value;
-            }
-
-            void instance_value_variant::construct(int_t value)
-            {
-                m_value.m_int = value;
-            }
-
-            void instance_value_variant::construct(double value)
-            {
-                m_value.m_double = value;
-            }
-
-            void instance_value_variant::construct(std::string value)
-            {
-                construct(m_value.m_string, std::move(value));
-            }
-
-            void instance_value_variant::construct(version_value value)
-            {
-                construct(m_value.m_version, std::move(value));
-            }
-
-            void instance_value_variant::construct(list_value value)
-            {
-                construct(m_value.m_list, std::move(value));
-            }
-
-            void instance_value_variant::construct(project_value value)
-            {
-                construct(m_value.m_project, std::move(value));
-            }
-
-            void instance_value_variant::construct(library_value value)
-            {
-                construct(m_value.m_library, std::move(value));
-            }
-
-            void instance_value_variant::construct(executable_value value)
-            {
-                construct(m_value.m_executable, std::move(value));
-            }
-
-            template <typename Value>
-            void instance_value_variant::construct(Value& destination, Value&& value)
-            {
-                new (&destination) Value{ std::move(value) };
-            }
-
-            void instance_value_variant::destruct()
-            {
-                switch (m_which)
-                {
-                    case which_t::bool_:
-                    case which_t::int_:
-                    case which_t::double_:
-                    case which_t::version:
-                        // Trivially destructible. Do nothing.
-                        break;
-                    case which_t::string:
-                    {
-                        call_dtor(m_value.m_string);
-                    } break;
-                    case which_t::list:
-                    {
-                        call_dtor(m_value.m_list);
-                    } break;
-                    case which_t::project:
-                    {
-                        call_dtor(m_value.m_project);
-                    } break;
-                    case which_t::library   :
-                    {
-                        call_dtor(m_value.m_library);
-                    } break;
-                    case which_t::executable:
-                    {
-                        call_dtor(m_value.m_executable);
-                    } break;
-                }
-            }
-
-            template <typename T>
-            void instance_value_variant::call_dtor(T& val)
-            {
-                val.~T();
+                m_value = std::move(value);
             }
 
 #define BINARY_OPERATOR(op) \
-if(m_which != rhs.which()) \
+if(which() != rhs.which()) \
 { \
     return false; \
 } \
  \
-switch (m_which) \
+switch (which()) \
 { \
     case which_t::bool_: return get_bool() op rhs.get_bool(); \
     case which_t::int_: return get_int() op rhs.get_int(); \
@@ -388,73 +155,76 @@ CMSL_UNREACHABLE("Unknown alternative")
 
             const version_value &instance_value_variant::get_version_cref() const
             {
-                return m_value.m_version;
+                return std::get<version_value>(m_value);
             }
 
             version_value &instance_value_variant::get_version_ref()
             {
-                return m_value.m_version;
+                return std::get<version_value>(m_value);
             }
 
             void instance_value_variant::set_version(version_value value)
             {
-                reassign(std::move(value), which_t::version);
+                m_value = std::move(value);
             }
 
             const list_value &instance_value_variant::get_list_cref() const
             {
-                return m_value.m_list;
+                return std::get<list_value>(m_value);
             }
 
             list_value &instance_value_variant::get_list_ref()
             {
-                return m_value.m_list;
+                return std::get<list_value>(m_value);
             }
 
             void instance_value_variant::set_list(list_value value)
             {
-                reassign(std::move(value), which_t::list);
+                m_value = std::move(value);
             }
 
             const project_value &instance_value_variant::get_project_cref() const
             {
-                return m_value.m_project;
+                return std::get<project_value>(m_value);
             }
 
             project_value &instance_value_variant::get_project_ref()
             {
-                return m_value.m_project;
+                return std::get<project_value>(m_value);
             }
 
             void instance_value_variant::set_project(project_value value)
             {
-                reassign(std::move(value), which_t::project);
+                m_value = std::move(value);
             }
 
             const library_value &instance_value_variant::get_library_cref() const
             {
-                return m_value.m_library;
+                return std::get<library_value>(m_value);
             }
 
             library_value &instance_value_variant::get_library_ref()
             {
-                return m_value.m_library;
+                return std::get<library_value>(m_value);
             }
 
-            void instance_value_variant::set_library(library_value value){
-                reassign(std::move(value), which_t::library);
+            void instance_value_variant::set_library(library_value value)
+            {
+                m_value = std::move(value);
             }
 
             const executable_value &instance_value_variant::get_executable_cref() const
             {
-                return m_value.m_executable;
+                return std::get<executable_value>(m_value);
             }
 
-            executable_value &instance_value_variant::get_executable_ref(){
-                return m_value.m_executable;
+            executable_value &instance_value_variant::get_executable_ref()
+            {
+                return std::get<executable_value>(m_value);
             }
 
-            void instance_value_variant::set_executable(executable_value value){
-                reassign(std::move(value), which_t::executable);
+            void instance_value_variant::set_executable(executable_value value)
+            {
+                m_value = std::move(value);
             }
 }
