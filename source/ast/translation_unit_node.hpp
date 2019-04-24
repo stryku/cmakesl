8e3ca7043453acc3324044d05b7130647cadb9d4
stryku <stryku2393@gmail.com>
2019-04-24 22:57:11 +0200

    Moved translation unit implementation to cpp file

--- source/ast/translation_unit_node.hpp
+++ source/ast/translation_unit_node.hpp
@@ -1,45 +1,26 @@
 #pragma once
 
 #include "ast/ast_node.hpp"
-#include "ast/ast_node_visitor.hpp" // Todo: to cpp
 
 #include <memory>
 #include <vector>
 
-#include <algorithm>
-
 namespace cmsl::ast
 {
-        class translation_unit_node : public ast_node
-        {
-        public:
-            using nodes_t = std::vector<std::unique_ptr<ast_node>>;
+    class translation_unit_node : public ast_node
+    {
+    public:
+        using nodes_t = std::vector<std::unique_ptr<ast_node>>;
 
-            explicit translation_unit_node(nodes_t nodes)
-                : m_nodes{ std::move(nodes)}
-            {}
+        explicit translation_unit_node(nodes_t nodes);
 
-            const nodes_t& nodes() const
-            {
-                return m_nodes;
-            }
+        const nodes_t& nodes() const;
 
-            void visit(ast_node_visitor& visitor) const override
-            {
-                visitor.visit(*this);
-            }
+        void visit(ast_node_visitor& visitor) const override;
+        source_location begin_location() const override;
+        source_location end_location() const override;
 
-            source_location begin_location() const override
-            {
-                return (!m_nodes.empty()) ? m_nodes.front()->begin_location() : source_location{};
-            }
-
-            source_location end_location() const override
-            {
-                return (!m_nodes.empty()) ? m_nodes.front()->end_location() : source_location{};
-            }
-
-        private:
-            nodes_t m_nodes;
-        };
+    private:
+        nodes_t m_nodes;
+    };
 }
