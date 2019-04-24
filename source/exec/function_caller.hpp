9c1921741830e5f963e42f1eae20581ddbdcafc2
stryku <stryku2393@gmail.com>
2019-04-24 22:25:05 +0200

    Removed unneeded ast stuff

--- source/exec/function_caller.hpp
+++ source/exec/function_caller.hpp
@@ -23,18 +23,6 @@ namespace cmsl
             class instances_holder_interface;
         }
 
-        // Todo: remove
-        class function_caller
-        {
-        public:
-            virtual ~function_caller() = default;
-
-            virtual inst::instance* call(const ast::function& fun, const std::vector<inst::instance*>& params) = 0;
-            virtual inst::instance* call_member(inst::instance& class_instance,
-                                                const ast::function& fun,
-                                                const std::vector<inst::instance*>& params) = 0;
-        };
-
         // Todo: change name
         class function_caller2
         {
