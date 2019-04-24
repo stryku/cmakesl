6dfb12c27dead68e01de931d34800a8bb97c01a4
stryku <stryku2393@gmail.com>
2019-04-24 21:16:24 +0200

    Improve indexer example

--- examples/index.c
+++ examples/index.c
@@ -2,14 +2,44 @@
 
 #include <stdio.h>
 
+const char* entry_type_to_string(enum cmsl_index_entry_type entry_type)
+{
+    switch(entry_type)
+    {
+
+        case type: return "type";
+        case identifier: return "identifier";
+        case parameter_declaration_identifier: return "parameter declaration identifier";
+        case class_member_identifier: return "class member identifier";
+        case operator_function: return "operator function";
+        case function_call_name: return "function call name";
+    }
+
+    return "unknown";
+}
+
 int main()
 {
-    const char* source = "class hello_complete_world {};"
-                         "int foo() {}"
+    const char* source = "class hello_world"
+                         "{"
+                         "    int foo_getter()"
+                         "    {"
+                         "        return foo;"
+                         "    }"
+                         ""
+                         "    int foo;"
+                         "};"
+                         ""
+                         "int foo() { return 0; }"
+                         ""
                          "int bar;"
-                         "int main(int param_1, list<string> param_2) {}"
-                         "int baz() {}"
-                         "class last_class {};";
+                         ""
+                         "int main(int param_1, list<string> param_2)"
+                         "{"
+                         "    hello_world hw;"
+                         "    hw.foo = 42;"
+                         "    return hw.foo_getter();"
+                         "}";
 
     struct cmsl_parsed_source* parsed_source = cmsl_parse_source(source);
     if(!parsed_source)
@@ -28,7 +58,7 @@ int main()
     for(i = 0; i < index_entries->num_entries; ++i)
     {
         struct cmsl_index_entry* entry = &index_entries->entries[i];
-        printf("source begin: %d, source end: %d, entry type: %d, destination position: %d\n", entry->begin_pos, entry->end_pos, entry->type, entry->position);
+        printf("source begin: %d, source end: %d, destination position: %d, entry type: %s\n", entry->begin_pos, entry->end_pos, entry->position, entry_type_to_string(entry->type));
     }
 
     cmsl_destroy_index_entries(index_entries);
