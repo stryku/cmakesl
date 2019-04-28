fadb3d2f73ab85ccd3db5cb5f4a6999f4559b19b
stryku <stryku2393@gmail.com>
2019-04-28 19:01:49 +0200

    Moved user_function_node methods implementation to cpp file

--- source/ast/user_function_node.hpp
+++ source/ast/user_function_node.hpp
@@ -1,84 +1,44 @@
 #pragma once
 
-#include "ast/block_node.hpp"
+#include "ast/ast_node.hpp"
 #include "ast/type_representation.hpp"
-#include "ast/ast_node_visitor.hpp" // todo: to cpp
 #include "ast/parameter_declaration.hpp"
-
-#include "lexer/token/token.hpp"
 
 #include <memory>
 
 namespace cmsl::ast
 {
-        class user_function_node : public ast_node
-        {
-        public:
-            using params_t = std::vector<param_declaration>;
+    class block_node;
 
-            user_function_node(type_representation return_type,
-                                token_t name,
-                                token_t open_paren,
-                                params_t params,
-                                token_t close_paren,
-                                std::unique_ptr<block_node> body)
-                    : m_return_type{ std::move(return_type) }
-                    , m_name{ name }
-                    , m_params{ std::move(params) }
-                    , m_body{ std::move(body) }
-            {}
+    class user_function_node : public ast_node
+    {
+    public:
+        using params_t = std::vector<param_declaration>;
 
-            type_representation return_type_representation() const
-            {
-                return m_return_type;
-            }
+        user_function_node(type_representation return_type,
+                           token_t name,
+                           token_t open_paren,
+                           params_t params,
+                           token_t close_paren,
+                           std::unique_ptr<block_node> body);
 
-            token_t name() const
-            {
-                return m_name;
-            }
+        const type_representation& return_type_representation() const;
+        token_t name() const;
+        token_t open_paren() const;
+        token_t close_paren() const;
+        const block_node& body() const;
+        const params_t& param_declarations() const;
 
-            token_t open_paren() const
-            {
-                return m_open_paren;
-            }
+        void visit(ast_node_visitor &visitor) const override;
+        source_location begin_location() const override;
+        source_location end_location() const override;
 
-            token_t close_paren() const
-            {
-                return m_close_paren;
-            }
-
-            const params_t& param_declarations() const
-            {
-                return m_params;
-            }
-
-            const block_node& body() const
-            {
-                return *m_body;
-            }
-
-            void visit(ast_node_visitor &visitor) const override
-            {
-                visitor.visit(*this);
-            }
-
-            source_location begin_location() const override
-            {
-                return m_return_type.tokens().front().src_range().begin;
-            }
-
-            source_location end_location() const override
-            {
-                return m_body->end_location();
-            }
-
-        private:
-            type_representation m_return_type;
-            token_t m_name;
-            token_t m_open_paren;
-            params_t m_params;
-            token_t m_close_paren;
-            std::unique_ptr<block_node> m_body;
-        };
+    private:
+        type_representation m_return_type;
+        token_t m_name;
+        token_t m_open_paren;
+        params_t m_params;
+        token_t m_close_paren;
+        std::unique_ptr<block_node> m_body;
+    };
 }
