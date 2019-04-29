0fc5cb024735b6ea704e5a9f6dd0f82a15474ca3
stryku <stryku2393@gmail.com>
2019-04-29 12:29:10 +0200

    Added no main reporting to global_executor

--- source/errors/errors_observer.cpp
+++ source/errors/errors_observer.cpp
@@ -20,16 +20,21 @@ namespace cmsl::errors
 {
         void errors_observer::nofify_error(const error& error)
         {
-            const auto range_start = error.range.begin.absolute - error.line_start_pos;
-            std::string range_indicator(range_start, ' ');
-            range_indicator += std::string(error.range.size(), '^' );
 
             std::cout << error.source_path << ':'
             << error.range.begin.line << ':'
             << error.range.begin.column << ": "
             << to_string(error.type) << ": "
-            << error.message << '\n'
-            << error.line_snippet << '\n'
-            << range_indicator << '\n';
+            << error.message << '\n';
+
+            if(!error.line_snippet.empty())
+            {
+                const auto range_start = error.range.begin.absolute - error.line_start_pos;
+                std::string range_indicator(range_start, ' ');
+                range_indicator += std::string(error.range.size(), '^' );
+
+                std::cout << error.line_snippet << '\n'
+                          << range_indicator << '\n';
+            }
         }
 }
