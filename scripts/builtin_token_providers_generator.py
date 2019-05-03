1d27fd465abb5a7f018da6b936235829a874720e
stryku <stryku2393@gmail.com>
2019-05-03 14:19:55 +0200

    Added all builtin types to token providers generator

--- scripts/builtin_token_providers_generator.py
+++ scripts/builtin_token_providers_generator.py
@@ -57,7 +57,181 @@ GENERATION_INFO = {
     'bool': {
         'tokens': {
             'name' : ['class bool', 'class ', 'bool'],
-            'default_constructor' : ['bool();', '', 'bool']
+            'default_constructor' : ['bool();', '', 'bool'],
+            'copy_constructor': ['bool(bool value);', '', 'bool'],
+            'conversion_from_int_constructor': ['bool(int value);', '', 'bool'],
+            'operator_equal': ['bool& operator=(bool rhs);', 'bool& operator', '='],
+            'operator_equal_equal': ['bool operator==(bool rhs);', 'bool operator', '=='],
+            'operator_pipe_pipe': ['bool operator||(bool rhs);', 'bool operator', '||'],
+            'operator_amp_amp': ['bool operator&&(bool rhs);', 'bool operator', '&&'],
+            'to_string': ['string to_string();', 'string ', 'to_string']
+        }
+    },
+    'int': {
+        'tokens': {
+            'name': ['class int', 'class ', 'int'],
+            'default_constructor': ['int();', '', 'int'],
+            'copy_constructor': ['int(int value);', '', 'int'],
+            'conversion_from_bool_constructor': ['int(bool value);', '', 'int'],
+            'conversion_from_double_constructor': ['int(double value);', '', 'int'],
+            'to_string': ['string to_string();', 'string ', 'to_string'],
+            'operator_plus': ['int operator+(int rhs);', 'int operator', '+'],
+            'operator_minus': ['int operator-(int rhs);', 'int operator', '-'],
+            'operator_star': ['int operator*(int rhs);', 'int operator', '*'],
+            'operator_slash': ['int operator/(int rhs);', 'int operator', '/'],
+            'operator_equal': ['int& operator=(int rhs);', 'int& operator', '='],
+            'operator_plus_equal': ['int& operator+=(int rhs);', 'int& operator', '+='],
+            'operator_minus_equal': ['int& operator-=(int rhs);', 'int& operator', '-='],
+            'operator_star_equal': ['int& operator*=(int rhs);', 'int& operator', '*='],
+            'operator_slash_equal': ['int& operator/=(int rhs);', 'int& operator', '/='],
+            'operator_less': ['bool operator<(int rhs);', 'bool operator', '<'],
+            'operator_less_equal': ['bool operator<=(int rhs);', 'bool operator', '<='],
+            'operator_greater': ['bool operator>(int rhs);', 'bool operator', '>'],
+            'operator_greater_equal': ['bool operator>=(int rhs);', 'bool operator', '>='],
+            'operator_equal_equal': ['bool operator==(int rhs);', 'bool operator', '=='],
+
+        }
+    },
+    'double': {
+        'tokens': {
+            'name': ['class double', 'class ', 'double'],
+            'default_constructor': ['double();', '', 'double'],
+            'copy_constructor': ['double(double value);', '',' double'],
+            'conversion_from_int_constructor': ['double(int value);', '', 'double'],
+            'operator_plus': ['double operator+(double value);', 'double operator', '+'],
+            'operator_minus': ['double operator-(double value);', 'double operator', '-'],
+            'operator_start': ['double operator*(double value);', 'double operator', '*'],
+            'operator_slash': ['double operator/(double value);', 'double operator', '/'],
+            'operator_equal': ['double& operator=(double value);', 'double& operator', '='],
+            'operator_plus_equal': ['double& operator+=(double value);', 'double& operator', '+='],
+            'operator_minus_equal': ['double& operator-=(double value);', 'double& operator', '-='],
+            'operator_start_equal': ['double& operator*=(double value);', 'double& operator', '*='],
+            'operator_slash_equal': ['double& operator/=(double value);', 'double& operator', '/='],
+            'operator_less': ['bool operator<(double value);', 'bool operator', '<'],
+            'operator_less_equal': ['bool operator<=(double value);', 'bool operator', '<='],
+            'operator_greater': ['bool operator>(double value);', 'bool operator', '>'],
+            'operator_greater_equal': ['bool operator>=(double value);', 'bool operator', '>='],
+            'to_string': ['string to_string();', 'string ', 'to_string']
+        }
+    },
+    'string': {
+        'tokens': {
+            'name': ['class string', 'class ', 'string'],
+            'default_constructor': ['string();', '', 'string'],
+            'copy_constructor': ['string(string value);', '', 'string'],
+            'value_initialization_constructor': ['string(string value, int count);', '', 'string'],
+            'empty': ['bool empty();', 'bool ', 'empty'],
+            'size': ['int size();', ' int ', 'size'],
+            'operator_equal_equal': ['bool operator==(string rhs);', 'bool operator', '=='],
+            'operator_not_equal': ['bool operator!=(string rhs);', 'bool operator', '!='],
+            'operator_less': ['bool operator<(string rhs);', 'bool operator', '<'],
+            'operator_less_equal': ['bool operator<=(string rhs);', 'bool operator', '<='],
+            'operator_greater': ['bool operator>(string rhs);', 'bool operator', '>'],
+            'operator_greater_equal': ['bool operator>=(string rhs);', 'bool operator', '>='],
+            'operator_plus': ['string operator+(string rhs);', 'string operator', '+'],
+            'operator_plus_equal': ['string& operator+=(string rhs);', 'string& operator', '+='],
+            'clear': ['void clear();', ' void ', 'clear'],
+            'insert': ['string& insert(int position, string str);', 'string& ', 'insert'],
+            'erase_pos': ['string& erase(int position);', 'string& ', 'erase'],
+            'erase_pos_count': ['string& erase(int position, int count);', 'string& ', 'erase'],
+            'starts_with': ['bool starts_with(string str);', 'bool ', 'starts_with'],
+            'ends_with': ['bool ends_with(string str);', 'bool ', 'ends_with'],
+            'replace': ['string& replace(int position, int count, string str);', 'string& ', 'replace'],
+            'substr_pos': ['string substr(int position);', 'string ', 'substr'],
+            'substr_pos_count': ['string substr(int position, int count);', 'string ', 'substr'],
+            'resize_size': ['void resize(int new_size);', 'void ', 'resize'],
+            'resize_size_fill': ['void resize(int new_size, string fill);', 'void ', 'resize'],
+            'find_str': ['int find(string str);', 'int ', 'find'],
+            'find_str_pos': ['int find(string str, int position);', 'int ', 'find'],
+            'find_not_of_str': ['int find_not_of(string str);', 'int ', 'find_not_of'],
+            'find_not_of_str_pos': ['int find_not_of(string str, int position);', 'int ', 'find_not_of'],
+            'find_last': ['int find_last(string str);', 'int ', 'find_last'],
+            'find_last_not_of': ['int find_last_not_of(string str);', 'int ', 'find_last_not_of'],
+            'contains': ['bool contains(string str);', 'bool ', 'contains'],
+            'lower': ['void lower();', 'void ', 'lower'],
+            'make_lower': ['string make_lower();', 'string ', 'make_lower'],
+            'upper': ['void upper();', 'void ', 'upper'],
+            'make_upper': ['string make_upper();', 'string ', 'make_upper']
+        }
+    },
+    'version': {
+        'tokens': {
+            'name': ['class version', 'class ', 'version'],
+            'constructor_major': ['version(int major);', '', 'version'],
+            'constructor_major_minor': ['version(int major, int minor);', '', 'version'],
+            'constructor_major_minor_patch': ['version(int major, int minor, int patch);', '', 'version'],
+            'constructor_major_minor_patch_tweak': ['version(int major, int minor, int patch, int tweak);', '', 'version'],
+            'operator_equal_equal': ['bool operator==(version rhs);', 'bool operator', '=='],
+            'operator_not_equal': ['bool operator!=(version rhs);', 'bool operator', '!='],
+            'operator_less': ['bool operator<(version rhs);', 'bool operator', '<'],
+            'operator_less_equal': ['bool operator<=(version rhs);', 'bool operator', '<='],
+            'operator_greater': ['bool operator>(version rhs);', 'bool operator', '>'],
+            'operator_greater_equal': ['bool operator>=(version rhs);', 'bool operator', '>='],
+            'major': ['int major();', 'int ', 'major'],
+            'minor': ['int minor();', 'int ', 'minor'],
+            'patch': ['int patch();', 'int ', 'patch'],
+            'tweak': ['int tweak();', 'int ', 'tweak'],
+            'to_string': ['string to_string();', 'string ', 'to_string']
+        }
+    },
+    'list': {
+        'tokens': {
+            'name': ['class list', 'class ', 'list'],
+            'default_constructor': ['list();', '', 'list'],
+            'push_back_value': ['void push_back(value_type value);', 'void ', 'push_back'],
+            'push_back_list': ['void push_back(list<value_type> value);', 'void ', 'push_back'],
+            'operator_plus_value': ['list<value_type> operator+(value_type value);', 'list<value_type> operator', '+'],
+            'operator_plus_list': ['list<value_type> operator+(list<value_type> value);', 'list<value_type> operator', '+'],
+            'operator_plus_equal_value': ['list<value_type>& operator+=(value_type value);', 'list<value_type>& operator', '+='],
+            'operator_plus_equal_list': ['list<value_type>& operator+=(list<value_type> value);', 'list<value_type>& operator', '+='],
+            'push_front_value': ['void push_front(value_type value);', 'void ', 'push_front'],
+            'push_front_list': ['void push_front(list<value_type> value);', 'void ', 'push_front'],
+            'pop_back': ['void pop_back();', 'void ', 'pop_back'],
+            'pop_front': ['void pop_front();', 'void ', 'pop_front'],
+            'at': ['value_type& at(int position);', 'value_type& ', 'at'],
+            'front': ['value_type front();', 'value_type ', 'front'],
+            'back': ['value_type back();', 'value_type ', 'back'],
+            'insert_pos_value': ['void insert(int position, value_type value);', 'void ', 'insert'],
+            'insert_pos_list': ['void insert(int position, list<value_type> value);', 'void ', 'insert'],
+            'erase_pos': ['void erase(int position);', 'void ', 'erase'],
+            'erase_pos_count': ['void erase(int position, int count);', 'void ', 'erase'],
+            'remove_value': ['int remove(value_type value);', 'int ', 'remove'],
+            'remove_value_count': ['int remove(value_type value, int count);', 'int ', 'remove'],
+            'remove_last': ['int remove_last(value_type value, int count);', 'int ', 'remove_last'],
+            'clear': ['void clear();', 'void ', 'clear'],
+            'resize': ['void resize(int new_size);', 'void ', 'resize'],
+            'sort': ['void sort();', 'void ', 'sort'],
+            'reverse': ['void reverse();', 'void ', 'reverse'],
+            'min': ['int min();', 'int ', 'min'],
+            'max': ['int max();', 'int ', 'max'],
+            'sublist_pos': ['list<value_type> sublist(int position);', 'list<value_type> ', 'sublist'],
+            'sublist_pos_count': ['list<value_type> sublist(int position, int count);', 'list<value_type> ', 'sublist'],
+            'size': ['int size();', 'int ', 'size'],
+            'empty': ['bool empty();', 'int ', 'empty'],
+            'find_value': ['int find(value_type value);', 'int ', 'find'],
+            'find_value_pos': ['int find(value_type value, int position);', 'int ', 'find']
+        }
+    },
+    'project': {
+        'tokens': {
+            'name': ['class project', 'class ', 'project'],
+            'constructor_name': ['project(string name);', '', 'project'],
+            'add_executable': ['executable add_executable(string name, list<string> sources);', 'executable ', 'add_executable'],
+            'library': ['library add_library(string name, list<string> sources);', 'library ', 'add_library']
+        }
+    },
+    'library': {
+        'tokens': {
+            'name': ['class library', 'class ', 'library'],
+            'method_name': ['string name();', 'string ', 'name'],
+            'link_to': ['void link_to(library lib);', 'void ', 'link_to']
+        }
+    },
+    'executable': {
+        'tokens': {
+            'name': ['class executable', 'class ', 'executable'],
+            'method_name': ['string name();', 'string ', 'name'],
+            'link_to': ['void link_to(library lib);', 'void ', 'link_to']
         }
     }
 }
@@ -79,7 +253,7 @@ def generate_providers():
     generated_providers = ''
 
     for type_name, type_dict in GENERATION_INFO.items():
-        print('Generating provider {}, {}...'.format(type_name, str(type_dict)))
+        print('Generating provider {}...'.format(type_name))
 
         type_documentation = load_type_documentation(type_name)
 
@@ -89,7 +263,7 @@ def generate_providers():
         methods_source = ''
 
         for token, token_search_info in type_dict['tokens'].items():
-            print('Generating provider token {}, {}...'.format(token, str(token_search_info)))
+            print('Generating provider {}::{}...'.format(type_name, token))
             method_source = TOKEN_PROVIDER_METHOD_TEMPLATE
             method_source = method_source.replace('%token_name%', token)
 
--- source/sema/builtin_token_provider.hpp
+++ source/sema/builtin_token_provider.hpp
@@ -7,6 +7,14 @@
 
 namespace cmsl::sema {
 struct bool_tokens_provider;
+struct double_tokens_provider;
+struct executable_tokens_provider;
+struct library_tokens_provider;
+struct list_tokens_provider;
+struct project_tokens_provider;
+struct int_tokens_provider;
+struct string_tokens_provider;
+struct version_tokens_provider;
 
 // An instance of this class has to live for the whole program execution.
 // String views to paths are used later by sema and execution.
@@ -17,11 +25,27 @@ public:
     std::optional<cmsl::string_view> builtin_documentation_path);
 
   bool_tokens_provider bool_() const;
+  double_tokens_provider double_() const;
+  executable_tokens_provider executable() const;
+  library_tokens_provider library() const;
+  list_tokens_provider list() const;
+  project_tokens_provider project() const;
+  int_tokens_provider int_() const;
+  string_tokens_provider string() const;
+  version_tokens_provider version() const;
 
 private:
   enum class builtin_type
   {
-    bool_
+    bool_,
+    double_,
+    executable,
+    library,
+    list,
+    project,
+    int_,
+    string,
+    version
   };
 
   void initialize_documentation_paths();
