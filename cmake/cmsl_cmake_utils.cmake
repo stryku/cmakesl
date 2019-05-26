49b958083093e4ddf4b22f3fe0b0d1ee0383d51d
stryku <stryku2393@gmail.com>
2019-05-26 12:47:14 +0200

    Used cmake_parse_arguments in utils code

--- cmake/cmsl_cmake_utils.cmake
+++ cmake/cmsl_cmake_utils.cmake
@@ -1,17 +1,23 @@
-function(cmsl_add_test NAME SOURCES INCLUDE_DIRS LIBRARIES_TO_LINK)
-    set(test_name ${NAME}_test)
+function(cmsl_add_test)
+    set(options)
+    set(oneValueArgs NAME)
+    set(multiValueArgs SOURCES INCLUDE_DIRS LIBRARIES)
+    cmake_parse_arguments(CMSL_ADD_TEST "${options}" "${oneValueArgs}"
+            "${multiValueArgs}" ${ARGN} )
 
-    add_executable(${test_name} ${SOURCES})
+    set(test_name ${CMSL_ADD_TEST_NAME}_test)
+
+    add_executable(${test_name} ${CMSL_ADD_TEST_SOURCES})
 
     target_link_libraries(${test_name}
         PRIVATE
             gmock_main
-            ${LIBRARIES_TO_LINK}
+            ${CMSL_ADD_TEST_LIBRARIES}
     )
 
     target_include_directories(${test_name}
         PRIVATE
-            ${INCLUDE_DIRS}
+            ${CMSL_ADD_TEST_INCLUDE_DIRS}
     )
 
     add_test(NAME ${test_name} COMMAND ${test_name})
